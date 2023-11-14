89f668143eec4cc3ce21157b1aa89bddfd8667fd
Anastasia Svist <agbelousova1@gmail.com>
2023-11-14 20:10:35 +0300

    Знакомсттво с с#

---
+++ Aqa_MTS/TypesAndOperators/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+{
+    /*
+    Console.WriteLine("Hello, World!");
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    string name;
+
+    name = "Bob";
+    */
+
+    //решение задачи
+    //подготовка данных
+
+    Console.Write();
+
+    bool isWhiteBreadFresh = true;
+    ushort whiteBreadPrice = 150;
+    ushort butterPrice = 230;
+    ushort milkPrice = 170;
+    float milkFatPercentage = 1.2F;
+
+
+}
+
+
