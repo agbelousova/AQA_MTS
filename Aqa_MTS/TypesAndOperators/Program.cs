495c9c3d3cf4f401bbcef2f3828e55cacbe5a435
Anastasia Svist <agbelousova1@gmail.com>
2023-11-16 19:27:24 +0300

    Finish

--- Aqa_MTS/TypesAndOperators/Program.cs
+++ Aqa_MTS/TypesAndOperators/Program.cs
@@ -12,18 +12,70 @@
     name = "Bob";
     */
 
+
+
+    /*Console.Write($"Ваш возраст:");
+    string? name = Console.ReadLine();*/
+
+    /*double x = 10;
+    double z = x % 4;
+
+    int x1 = 5;
+    int z1 = --x1; //z1 = 4; x1=4
+    Console.WriteLine($"x1= {x1} z1= {z1});*/
+
+    
+    
     //решение задачи
     //подготовка данных
-
-    Console.Write();
-
+    
     bool isWhiteBreadFresh = true;
     ushort whiteBreadPrice = 150;
     ushort butterPrice = 230;
     ushort milkPrice = 170;
     float milkFatPercentage = 1.2F;
+    ushort icecreamPrice = 250;
 
+    Console.Write("Введите начальную сумму: ");
+    string? sumString = Console.ReadLine();
+    short sum = Convert.ToInt16(sumString);
 
+    if (sum > 0)
+    {
+        if (!isWhiteBreadFresh)
+        {
+            Console.WriteLine("Батон не свежий");
+        } 
+        else if (sum >= whiteBreadPrice)
+            {
+                sum = Convert.ToInt16(sum - whiteBreadPrice);
+                Console.WriteLine($"Купли свежий батон по цене {whiteBreadPrice}");
+            }
+        else
+        {
+            Console.WriteLine("на батон денег нет");
+        }
+
+        sum = (short)(sum - butterPrice);
+
+        if (milkFatPercentage == 1.2F)
+        {
+            sum =Convert.ToInt16(sum - milkPrice);
+        }
+
+        if (sum >= icecreamPrice)
+        {
+            sum =Convert.ToInt16(sum - icecreamPrice);
+        }
+    
+        Console.WriteLine($"Остаток суммы: {sum}");
+    }
+    else
+    {
+        Console.Write("Сумма не может быть меньше 0");
+    }
+    
+   
 }
 
 
