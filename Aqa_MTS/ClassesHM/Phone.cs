a4adf039e7aa31dd4c8c0b555eac197da5e568fb
Anastasia Svist <agbelousova1@gmail.com>
2023-11-30 23:48:05 +0300

    Сделала первую задачу

---
+++ Aqa_MTS/ClassesHM/Phone.cs
@@ -0,0 +1,50 @@
+﻿namespace ClassesHM;
+
+public class Phone
+{
+    public string number;
+    public string model;
+    public int weight;
+
+    public Phone()
+    {
+        number = "+7(950)-300-5010";
+        model = "apple";
+        weight = 200;
+    }
+
+    public Phone(string number, string model)
+    {
+        this.number = number;
+        this.model = model;
+    }
+
+    public Phone(string number, string model, int weight) : this(number, model)
+    {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    public void receiveCall(string name)
+    {
+        Console.Write($"Звонит {name}: ");
+    }
+
+    public string getNumber()
+    {
+        Random rand = new Random();
+        int num = rand.Next(000, 999);
+        int num2 = rand.Next(00, 99);
+        int num3 = rand.Next(00, 99);
+        return number = "+7 (987)" + " " + num.ToString() + " " + num2.ToString() + " " + num3.ToString();
+    }
+
+    public void sendMessage(params string[] numbers)
+    {
+        foreach (var num in numbers)
+        {
+            Console.WriteLine($"Сообщение отправлено: {num}");
+        }
+    }
+}
---
+++ Aqa_MTS/ClassesHM/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ClassesHM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone[] phones = new Phone[3];
+            //три экземпляра класса используя разные конструкторы
+            phones[0] = new Phone();
+            phones[1] = new Phone("+7(960)-500-1010", "google");
+            phones[2] = new Phone("+7(960)-500-1010", "google", 150);
+            //Вывод на консоль значения их переменных
+            foreach (var phone in phones)
+            {
+                Console.Write($"Телефон: {phone.number}, Модель: {phone.model}, Вес: {phone.weight}");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            string[] name = { "Саша", "Лена", "Антон" };
+
+            foreach (var call in name)
+            {
+                new Phone().receiveCall(call);
+                Console.WriteLine(new Phone().getNumber());
+            }
+            Console.WriteLine();
+
+            string[] phonesMess1 = new string[2];
+            for (int i = 0; i < 2; i++)
+            {
+                phonesMess1[i] = new Phone().getNumber();
+                new Phone().sendMessage(phonesMess1[i]);
+            }
+            Console.WriteLine();
+            
+            string[] phonesMess2 = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                phonesMess2[i] = new Phone().getNumber();
+                new Phone().sendMessage(phonesMess2[i]);
+            }
+        }
+    }
+}
