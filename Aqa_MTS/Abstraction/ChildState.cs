1bce1b26b9ef9a8e31bb5ea4006f00bba8e10b11
Anastasia Svist <agbelousova1@gmail.com>
2023-12-05 14:35:23 +0300

    Updated

---
+++ Aqa_MTS/Abstraction/ChildState.cs
@@ -0,0 +1,12 @@
+﻿using Encapsulation;
+
+namespace Abstraction;
+
+public class ChildState : State
+{
+    void Test()
+    {
+        base.PrintProtectedInternal();  //метод из проекта Encapsulation из класса State
+    }
+    
+}
