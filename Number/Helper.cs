cf61d221c9ddb433a2e684348e53ba12eca4a2a0
Anastasia Svist <agbelousova1@gmail.com>
2023-12-25 21:05:07 +0300

    Finish

---
+++ Number/Helper.cs
@@ -0,0 +1,13 @@
+﻿namespace Number;
+using System.Collections;
+internal static class Helper
+{
+    public static void PrintCollection(IEnumerable collection)
+    {
+        foreach (var item in collection)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Number/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ Задание 3:
+Коллекция содержит набор чисел в виде одной строки "1, 2, 3, 4, 4, 5". Избавиться от повторяющихся элементов в строке и вывести результат на экран.
+ */
+
+using System.Collections;
+using Number;
+
+LinkedList<int> numberList = new LinkedList<int>();
+numberList.AddLast(1);
+numberList.AddLast(2);
+numberList.AddLast(3);
+numberList.AddLast(4);
+numberList.AddLast(4);
+numberList.AddLast(5);
+
+Console.WriteLine("Начальная строка:");
+Helper.PrintCollection(numberList);
+
+LinkedListNode<int> currentNode = numberList.First;
+
+while (currentNode.Next != null)
+{
+    if (currentNode.Value != currentNode.Next.Value) currentNode = currentNode.Next;
+    else numberList.Remove(currentNode.Value);
+}
+
+Console.WriteLine("Строка после удаления повторений:");
+Helper.PrintCollection(numberList);
