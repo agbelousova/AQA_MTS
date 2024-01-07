65b03d55b4a8f6a787d14218b6a799cb63417404
Anastasia Svist <agbelousova1@gmail.com>
2024-01-07 17:50:08 +0300

    End 1

---
+++ Aqa_MTS/Сoordinates/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ Задание 1:
+Реализовать класс Point, который определяет точку на координатной плоскости. В классе реализовать:
+внутренние поля x, y;
+конструктор с 2 параметрами;
+свойства доступа к внутренним полям класса;
+метод, выводящий значения внутренних полей класса.
+*/
+
+using Сoordinates;
+
+Random random = new Random();
+Point<int> pointInt = new Point<int>(random.Next(100), random.Next(100));
+Point<double> pointFloat = new Point<double>(random.NextDouble(), random.NextDouble());
+
+Console.WriteLine("Координаты точек:");
+
+pointInt.PrintValues();
+pointFloat.PrintValues();
