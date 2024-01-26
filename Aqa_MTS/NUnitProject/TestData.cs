14e6078ac4c5b485b6917199ec9cc250de9e2292
Anastasia Svist <agbelousova1@gmail.com>
2024-01-26 19:43:09 +0300

    End
    Signed-off-by: Anastasia Svist <agbelousova1@gmail.com>

--- Aqa_MTS/NUnitProject/TestData.cs
+++ Aqa_MTS/NUnitProject/TestData.cs
@@ -6,6 +6,9 @@ public class TestData
     {
         new object[] { 12, 3, 4 },
         new object[] { 12, 2, 6 },
-        new object[] { 12, 4, 3 }
+        new object[] { 12, 4, 3 },
+        new object[] { 12, 0, 0 },
+        new object[] { 0, 10, 0 },
+        new object[] { 10, 3, 3.33}
     };
 }
