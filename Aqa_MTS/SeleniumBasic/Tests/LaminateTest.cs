493c52cd85e7684da5d40115e848b9dbab220e48
Anastasia Svist <agbelousova1@gmail.com>
2024-01-31 20:26:23 +0300

    Fixed HM
    Signed-off-by: Anastasia Svist <agbelousova1@gmail.com>

--- Aqa_MTS/SeleniumBasic/Tests/LaminateTest.cs
+++ Aqa_MTS/SeleniumBasic/Tests/LaminateTest.cs
@@ -1,7 +1,7 @@
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumBasic.Tests;
-
 public class LaminateTest : BaseTest
 {
     [Test]
@@ -10,18 +10,42 @@ public class LaminateTest : BaseTest
         Driver.Navigate().GoToUrl("https://home-ex.ru/calculation/");
 
         IWebElement roomLength = Driver.FindElement(By.Id("ln_room_id"));
-        roomLength.Clear();
-        roomLength.SendKeys("100");
         IWebElement roomWidth = Driver.FindElement(By.Id("wd_room_id"));
-        roomWidth.Clear();
-        roomWidth.SendKeys("200");
+        IWebElement lenghtPlashki = Driver.FindElement(By.Id("ln_lam_id"));
+        IWebElement widthPlashki = Driver.FindElement(By.Id("wd_lam_id"));
+        IWebElement quantityPlashki = Driver.FindElement(By.Id("n_packing"));
+        IWebElement areaPacking = Driver.FindElement(By.Id("area"));
+        IWebElement layingLaminate  = Driver.FindElement(By.CssSelector("label[for=direction-laminate-id1]"));
+        IWebElement selectWebElement2 = Driver.FindElement(By.Id("laying_method_laminate"));
         
-        Driver.FindElement(By.Id("btn_calculate")).Click();
+        roomLength.Clear();
+        roomLength.SendKeys("1500");
+        roomWidth.Clear();
+        roomWidth.SendKeys("2500");
+        lenghtPlashki.Clear();
+        lenghtPlashki.SendKeys("1500");
+        widthPlashki.Clear();
+        widthPlashki.SendKeys("400");
+        quantityPlashki.Clear();
+        quantityPlashki.SendKeys("100");
+        areaPacking.SendKeys("300");
+        layingLaminate.Click();
+        SelectElement selectCrSize = new SelectElement(selectWebElement2);
+        selectCrSize.SelectByValue("0");
         
         Thread.Sleep(2000);
+        Driver.FindElement(By.Id("btn_calculate")).Click();
+        Thread.Sleep(2000);
         
-        Assert.That(Driver.FindElement(By.CssSelector(".calc-result")).Text, 
-            Is.EqualTo("Требуемое количество плашек ламината: 10\nКоличество упаковок ламината: 1\nСтоимость ламината: NaN руб\nВес ламината: 0 кг"));
+        string result = Driver.FindElement(By.CssSelector(".calc-result")).Text;
+
+        if (result.Contains("плашек ламината: 647") &&
+            result.Contains("упаковок ламината: 7") &&
+            result.Contains("0 руб") &&
+            result.Contains("0 кг"))
+            Assert.Pass();
+        
+      //  Assert.That(Driver.FindElement(By.CssSelector(".calc-result")).Text, 
+      //  Is.EqualTo("Требуемое количество плашек ламината: 9\r\nКоличество упаковок ламината: 1\r\nСтоимость ламината: 0 руб\r\nВес ламината: 0 кг"));
     }
-    
 }
