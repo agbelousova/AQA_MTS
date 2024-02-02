9b9982109188cee6642256a09a6249e1860660ab
Anastasia Svist <agbelousova1@gmail.com>
2024-02-02 22:17:17 +0300

    Init
    Signed-off-by: Anastasia Svist <agbelousova1@gmail.com>

--- Aqa_MTS/LINQ_HM/IntList.cs
+++ Aqa_MTS/LINQ_HM/IntList.cs
@@ -10,7 +10,7 @@ public class IntList
     {
         Console.WriteLine($"Начальная коллекция чисел:");
         PrintHelper.Print(_intSet);
-        var newIntSet = _intSet.Where(number => number % 2 != 0).Select(number => number).Distinct();
+        var newIntSet = _intSet.Where(number => number % 2 != 0).Distinct();
         Console.WriteLine($"\nНовая коллекция чисел:");
         PrintHelper.Print(newIntSet);
     }
